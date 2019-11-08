98ca26346b48d7b52a4a8a26b8952fc17e6c0632
james-pope <jamespope@billericak12.com>
2019-11-08 12:28:07 -0500

    working

--- src/Die.java
+++ src/Die.java
@@ -1,9 +1,19 @@
 
 public class Die {
+    private int dots;
 
+    public void roll(){
+        double die1 = (Math.random() * (6-1)+1)-1;
+        double die2 = (Math.random() * (6-1)+1)-1;
+        int result = (int) (die1 + die2);
+        dots = result;
+    }
+    public int getNumDots(){
+        return dots;
+    }
     public static void main(String [] args){
         Die die = new Die();
-        die.roll;
+        die.roll();
         System.out.println(die.getNumDots());
     }
 }
